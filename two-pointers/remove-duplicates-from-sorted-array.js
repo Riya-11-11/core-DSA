f6818190981afa1b72d8234a04c7863a8be40fed
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-05-23 17:44:25 +0530

    Add implementation to remove duplicates from sorted array using two pointers technique

---
+++ two-pointers/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,34 @@
+// Another example:
+
+// Input:
+// [0,0,1,1,1,2,2,3,3,4]
+
+// After removing duplicates:
+
+// [0,1,2,3,4,_,_,_,_,_]
+
+// Unique count:
+// 5
+
+// Answer:
+// 5
+
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+let l = 0;
+let r = 1;
+
+while (r < nums.length) {
+  let isUnique = true;
+  if (nums[l] === nums[r]) {
+    isUnique = false;
+    r++;
+  }
+
+  if (isUnique) {
+    nums[l + 1] = nums[r];
+    l++;
+    r++;
+  }
+}
+
+console.log(l+1);
