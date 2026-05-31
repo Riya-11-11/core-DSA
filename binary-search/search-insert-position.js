46836ee80ec7f425879b04435162eb7f3115f5b4
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-05-31 17:34:31 +0530

    Refactor binary search implementation for clarity and consistency

--- binary-search/search-insert-position.js
+++ binary-search/search-insert-position.js
@@ -5,26 +5,26 @@
 // Output:
 // 2
 
-let nums = [1,3,5,6];
+let nums = [1, 3, 5, 6];
 let target = 5;
 
-let left = 0;
-let right = nums.length-1;
-let mid = Math.floor((left+right)/2);
+let start = 0;
+let end = nums.length - 1;
+let mid = Math.floor((start + end) / 2);
 
-let ans = -1;
-
-while (left<=right) {
-    if (target===nums[mid]) {
-        ans = mid;
-        break;
-    }else if(target>nums[mid]){
-        left = mid+1;
-        mid = Math.floor((left+right)/2);
-    }else{
-        right = mid-1;
-        mid = Math.floor((left+right)/2);
-    }
+while (start <= end) {
+  if (target === nums[mid]) {
+    console.log(mid);
+    return;
+  } else if (target > nums[mid]) {
+    start = mid + 1;
+    mid = Math.floor((start + end) / 2);
+  } else {
+    end = mid - 1;
+    mid = Math.floor((start + end) / 2);
+  }
 }
 
-console.log(ans);
+console.log(start);
+
+
