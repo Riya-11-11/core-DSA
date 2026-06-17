432430543f205a039e43db9305b3c8901c4f685a
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-17 16:41:58 +0530

    Add implementation for finding the first occurrence of a character in a string using recursion

---
+++ recursion/first-occ.js
@@ -0,0 +1,28 @@
+// Find the First Occurrence of a Character
+
+// Input:
+// str = "banana"
+// ch = "a"
+
+// Output:
+// 1
+
+let str = "banana";
+let ch = "a";
+
+let idx = 0;
+
+function firstOcc(str, idx) {
+  if (idx === str.length) {
+    //We checked every character and never found ch.
+    return -1;
+  }
+
+  if (str[idx] === ch) {
+    return idx;
+  } else {
+    return firstOcc(str, idx + 1);
+  }
+}
+
+console.log(firstOcc(str, idx));
