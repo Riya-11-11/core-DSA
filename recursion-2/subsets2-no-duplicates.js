e4850d52b1c9f7317a896f06100d652959bc8cac
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-20 16:34:36 +0530

    Add implementation for generating all unique subsets of an array using recursion

---
+++ recursion-2/subsets2-no-duplicates.js
@@ -0,0 +1,33 @@
+let arr = [1, 1, 1];
+let ans = [];
+let allSubset = [];
+
+arr.sort((a, b) => a - b);
+
+function printSubset(arr, ans, allSubset, i) {
+  if (i === arr.length) {
+    allSubset.push([...ans]);
+    return;
+  }
+
+  //including-part...
+  ans.push(arr[i]);
+  printSubset(arr, ans, allSubset, i + 1);
+
+  //backtracking...
+  ans.pop();
+
+  //skipping...
+  let idx = i + 1;
+  while (idx < arr.length && arr[idx] === arr[idx - 1]) {
+    idx = idx + 1;
+  }
+
+  //exluding-part...
+  printSubset(arr, ans, allSubset, idx);
+}
+
+printSubset(arr, ans, allSubset, 0);
+console.log(allSubset);
+
+// node subsets2-no-duplicates.js
