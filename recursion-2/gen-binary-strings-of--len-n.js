526a22887eb4f4573612ac89d533638fa36632e6
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-24 12:23:13 +0530

    Refactor genBin function to remove unused parameter and simplify recursive calls

--- recursion-2/gen-binary-strings-of--len-n.js
+++ recursion-2/gen-binary-strings-of--len-n.js
@@ -14,7 +14,7 @@
 let n = 3;
 let ans = [];
 
-function genBin(ans, i) {
+function genBin(ans) {
   if (ans.length === n) {
     console.log(ans);
     return;
@@ -22,17 +22,17 @@ function genBin(ans, i) {
 
   //include choose 0
   ans.push(0);
-  genBin(ans, i + 1);
+  genBin(ans);
 
   //backtrack
   ans.pop();
 
   //include choose 1
   ans.push(1);
-  genBin(ans, i + 1);
+  genBin(ans);
 
   //backtrack
   ans.pop();
 }
 
-genBin(ans, 0);
+genBin(ans);
