146dde608e6166a64cf31ac721dc6603fecd2e25
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-06-30 16:31:29 +0530

    Add implementation to print only permutations where adjacent difference is 1

---
+++ recursion-2/Permutations/permutation-adjacent-diff-1.js
@@ -0,0 +1,56 @@
+// Print Only Permutations Where Adjacent Difference is 1
+
+// Input:
+// arr = [1,2,3]
+
+// Output:
+// 123
+// 321
+
+// Because:
+// |1-2| = 1
+// |2-3| = 1
+
+// and
+// |3-2| = 1
+// |2-1| = 1
+
+// Not:
+// 132
+
+// because:
+// |1-3| = 2
+
+// Hint: Check the condition only at the base case.
+
+let arr = [1, 2, 3];
+let path = [];
+let visited = [false, false, false];
+
+function permutationWithDiffOne() {
+  if (path.length === arr.length) {
+    for (let j = 0; j < path.length-1; j++) {
+      let diff = path[j] - path[j+1];
+      if (Math.abs(diff) !== 1) {
+        return;
+      }
+    }
+    console.log(path);
+    return;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!visited[i]) {
+      visited[i] = true;
+      path.push(arr[i]);
+
+      permutationWithDiffOne();
+
+      //backtrack
+      path.pop();
+      visited[i] = false;
+    }
+  }
+}
+
+permutationWithDiffOne();
